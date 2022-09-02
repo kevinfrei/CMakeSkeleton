433eb14ec2eebc425b0c26be2f45273deafa85c7
Kevin Frei <kevinfrei@hotmail.com>
2022-09-01 20:52:41 -0700

    Updated the skeleton to use better CMake standards.

--- demo2.cpp
+++ demo2.cpp
@@ -1,7 +1,36 @@
+#include <iostream>
+
+#include "demo_version_private.h"
 #include "dyn-public.h"
 #include "stat-public.h"
 
 void app(void) {
   stat1("app1");
   dyn2("app2");
+  std::cout << "Demo Project Version: " << DEMO_VERSION_STR << std::endl;
+#if defined(HOWDY)
+  std::cout << "Howdy! [Should see this]" << std::endl;
+#else
+  std::cout << "Howdy... [Shouldn't see this]" << std::endl;
+#endif
+#if defined(S_HOWDY)
+  std::cout << "SHowdy! [Should see this]" << std::endl;
+#else
+  std::cout << "SHowdy... [Shouldn't see this]" << std::endl;
+#endif
+#if defined(S_HOLA)
+  std::cout << "SHola! [Should see this]" << std::endl;
+#else
+  std::cout << "SHola... [Shouldn't see this]" << std::endl;
+#endif
+#if defined(S_BONJOUR)
+  std::cout << "SBonjour... [Shouldn't see this]" << std::endl;
+#else
+  std::cout << "SBonjour! [Should see this]" << std::endl;
+#endif
+#if defined(D_HOWDY)
+  std::cout << "DHowdy! [Should see this]" << std::endl;
+#else
+  std::cout << "DHowdy... [Shouldn't see this]" << std::endl;
+#endif
 }
